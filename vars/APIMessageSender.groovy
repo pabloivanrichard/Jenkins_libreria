ad40f207043fe81eff6ebd6dfba4683dd26365e6
Pablo Richard <pablorichard885@hotmail.com>
2023-07-20 10:52:56 -0300

    Agrego Lib

---
+++ vars/APIMessageSender.groovy
@@ -0,0 +1,16 @@
+def call(String url, String message) {
+    def http = new URL(url).openConnection()
+    http.setRequestMethod("POST")
+    http.setRequestProperty("Content-Type", "application/json")
+    http.setDoOutput(true)
+    
+    def data = [message: message] as JSON
+    
+    http.getOutputStream().withCloseable { os ->
+        os.write(data.toString().getBytes("UTF-8"))
+        os.flush()
+    }
+    
+    def response = http.getResponseCode()
+    println("API Response: $response")
+}
